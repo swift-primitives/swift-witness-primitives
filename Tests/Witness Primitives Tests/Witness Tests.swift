f967a9f86b1b7c84980c7ceb1c562e6648c0c481
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-08-01 01:37:58 +0200

    Fix compound suite/type naming to canonical forms

--- Tests/Witness Primitives Tests/Witness Tests.swift
+++ Tests/Witness Primitives Tests/Witness Tests.swift
@@ -18,7 +18,7 @@ extension Witness {
     @Suite
     struct Test {
         @Suite struct Unit {}
-        @Suite struct EdgeCase {}
+        @Suite struct `Edge Case` {}
     }
 }
 
@@ -29,28 +29,28 @@ extension Witness.Test.Unit {
     func `namespace exists and can be used for type containment`() {
         func acceptWitnessProtocol<T: Witness.`Protocol`>(_ type: T.Type) {}
 
-        struct ManualWitness: Witness.`Protocol` {
+        struct Fixture: Witness.`Protocol` {
             var operation: @Sendable () -> Void
         }
 
-        acceptWitnessProtocol(ManualWitness.self)
+        acceptWitnessProtocol(Fixture.self)
     }
 
     @Test
     func `Witness.Protocol is a pure marker protocol with no requirements`() {
-        struct MinimalWitness: Witness.`Protocol` {}
+        struct Fixture: Witness.`Protocol` {}
 
-        let _: any Witness.`Protocol` = MinimalWitness()
+        let _: any Witness.`Protocol` = Fixture()
     }
 }
 
 // MARK: - Edge Cases
 
-extension Witness.Test.EdgeCase {
+extension Witness.Test.`Edge Case` {
     @Test
     func `__WitnessProtocol typealias exists for macro use`() {
         func accept<T: __WitnessProtocol>(_ type: T.Type) {}
-        struct TestWitness: __WitnessProtocol {}
-        accept(TestWitness.self)
+        struct Fixture: __WitnessProtocol {}
+        accept(Fixture.self)
     }
 }
